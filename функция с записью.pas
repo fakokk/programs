34fbba1dcbd465dd4eca5f5cf3a1c5e8b69b7e97
fakokk <72958061+fakokk@users.noreply.github.com>
2021-03-25 00:26:14 +0900

    function with recording

---
+++ функция с записью.pas
@@ -0,0 +1,19 @@
+﻿type func = record
+  a: integer;
+  f: real; //значение функции
+  c: string; //название функции
+end;
+  
+var
+  parabola: func;
+begin
+  writeln('Введите значение аргумента:');
+  readln(parabola.a); //принимаем значение аргумента от пользователя
+  
+  parabola.f := sqr(parabola.a); //значение функции для функции y = x^2 (парабола)
+  parabola.c := 'парабола';
+  
+  writeln('Значение аргумента: ',parabola.a);
+  writeln('Значение функции: ',parabola.f);
+  writeln('Название функции: ',parabola.c);
+end.
